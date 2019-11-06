2b90921330aebdd5d5e5f975e617438628aa42f2
Rebecca <r.ayton@warwick.ac.uk>
2019-11-06 12:09:39 +0000

    Inventory player model display code added to new gui

--- src/java/com/reblou/cavemapper/MapGui.java
+++ src/java/com/reblou/cavemapper/MapGui.java
@@ -1,9 +1,14 @@
 package com.reblou.cavemapper;
 
-import java.io.IOException;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EntityLivingBase;
 
 public class MapGui extends GuiScreen {
 	private GuiButton testButton;
@@ -12,7 +17,10 @@ public class MapGui extends GuiScreen {
 	@Override
 	public void initGui() {
 		super.initGui();
-		this.buttonList.add(testButton = new GuiButton(0, this.width/2, this.height/2, "Foo"));
+//		this.buttonList.add(testButton = new GuiButton(0, this.width/2-100, this.height/2-10, "Foo"));
+		this.allowUserInput = true;
+
+		
 	}
 
 	@Override
@@ -27,10 +35,53 @@ public class MapGui extends GuiScreen {
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		this.drawDefaultBackground();
 		super.drawScreen(mouseX, mouseY, partialTicks);
+
+		int cx = this.width/2;
+		int cy = this.height/2;
+        drawPlayer(cx + 51, cy + 75, 30, (float)(cx + 51) - ((float) this.width), (float)(cy + 75 - 50) - ((float) this.height), this.mc.thePlayer);
 	}
 	
 	@Override
 	public boolean doesGuiPauseGame() {
 		return false;
 	}
+	
+	
+
+    public static void drawPlayer(int p_147046_0_, int p_147046_1_, int p_147046_2_, float p_147046_3_, float p_147046_4_, EntityLivingBase p_147046_5_)
+    {
+        GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float)p_147046_0_, (float)p_147046_1_, 50.0F);
+        GL11.glScalef((float)(-p_147046_2_), (float)p_147046_2_, (float)p_147046_2_);
+        GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+        float f2 = p_147046_5_.renderYawOffset;
+        float f3 = p_147046_5_.rotationYaw;
+        float f4 = p_147046_5_.rotationPitch;
+        float f5 = p_147046_5_.prevRotationYawHead;
+        float f6 = p_147046_5_.rotationYawHead;
+        GL11.glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
+        RenderHelper.enableStandardItemLighting();
+        GL11.glRotatef(-135.0F, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(-((float)Math.atan((double)(p_147046_4_ / 40.0F))) * 20.0F, 1.0F, 0.0F, 0.0F);
+        p_147046_5_.renderYawOffset = (float)Math.atan((double)(p_147046_3_ / 40.0F)) * 20.0F;
+        p_147046_5_.rotationYaw = (float)Math.atan((double)(p_147046_3_ / 40.0F)) * 40.0F;
+        p_147046_5_.rotationPitch = -((float)Math.atan((double)(p_147046_4_ / 40.0F))) * 20.0F;
+        p_147046_5_.rotationYawHead = p_147046_5_.rotationYaw;
+        p_147046_5_.prevRotationYawHead = p_147046_5_.rotationYaw;
+        GL11.glTranslatef(0.0F, p_147046_5_.yOffset, 0.0F);
+        RenderManager.instance.playerViewY = 180.0F;
+        RenderManager.instance.renderEntityWithPosYaw(p_147046_5_, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
+        p_147046_5_.renderYawOffset = f2;
+        p_147046_5_.rotationYaw = f3;
+        p_147046_5_.rotationPitch = f4;
+        p_147046_5_.prevRotationYawHead = f5;
+        p_147046_5_.rotationYawHead = f6;
+        GL11.glPopMatrix();
+        RenderHelper.disableStandardItemLighting();
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+        OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
+    }
 }
